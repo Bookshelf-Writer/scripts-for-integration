c14e5e24a497c295f6ae69812bbfb213abbaea2e
SUNsung <mail@embedded.biz.ua>
2024-07-18 18:46:28 +0200

    Update creator_const_json.sh

--- _run/scripts/creator_const_json.sh
+++ _run/scripts/creator_const_json.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # Original source: https://github.com/Bookshelf-Writer/scripts-for-integration/blob/main/_run/scripts/creator_const_json.sh
+# bash "$script_dir/creator_const_json.sh"
 
 dir_path=""
 file_name="const.json"
